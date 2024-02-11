130b966e2785ac10479255f29fca0e122e68aa9f
Saurabh <saurabhprogram@gmail.com>
2024-02-11 15:14:22 +0530

    First data pull

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array() );
+        wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', array(), '5.3.2' );
+        wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>', array(), '5.3.2' );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+
+// END ENQUEUE PARENT ACTION
+?>
---
+++ header.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content">
+		<?php
+		/* translators: Hidden accessibility text. */
+		_e( 'Skip to content', 'twentynineteen' );
+		?>
+	</a>
+
+	<header>
+        <div class="upper-head clearfix">
+            <div class="container">
+                <div class="contact-info">
+                    <p>
+                        <i class="flaticon-phone-call"></i> Phone: (012)-345-6789
+                    </p>
+                    <!-- <p>
+                        <i class="flaticon-mail"></i> Mail:
+                        <a href="https://htmldesigntemplates.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="b5c1dac0c7dbc1c7d4c3d0d9f5c1d0c6c1d8d4dcd99bd6dad8">[email&#160;protected]</a>
+                    </p> -->
+                </div>
+                <!-- <div class="login-btn pull-right">
+                    <a href="login.html"><i class="fa fa-user-plus"></i> Register</a>
+                    <a href="login.html"><i class="fa fa-unlock-alt"></i> Login</a>
+                </div> -->
+            </div>
+        </div>
+    </header>
+		<div class="navigation">
+        <div class="container">
+            <div class="navigation-content">
+                <div class="header_menu">
+
+                    <nav class="navbar navbar-default navbar-sticky-function navbar-arrow">
+                        <div class="logo pull-left">
+												<?php
+														$custom_logo_id = get_theme_mod('theme_logo');
+														if ($custom_logo_id) {
+																$logo_url = wp_get_attachment_image_url($custom_logo_id, 'full');
+																echo '<img src="' . esc_url($logo_url) . '" alt="Custom Logo">';
+														} else {
+																echo '<h1>' . get_bloginfo('name') . '</h1>';
+														}
+												?>
+
+                            <!-- <a href="index.html"><img alt="Image" src="<?php //echo get_template_directory_uri(); ?>/images/Yatra-01.png"></a> -->
+                        </div>
+												<div id="navbar" class="navbar-nav-wrapper">
+														<ul class="nav navbar-nav" id="responsive-menu">
+																<?php
+																$menu_items = wp_get_nav_menu_items('custom_menu');
+
+																foreach ($menu_items as $menu_item) {
+																		$menu_item_classes = implode(' ', $menu_item->classes);
+																		$menu_item_url = esc_url($menu_item->url);
+																		$menu_item_title = esc_html($menu_item->title);
+
+																		echo '<li class="' . $menu_item_classes . '">';
+																		echo '<a href="' . $menu_item_url . '">' . $menu_item_title . ' <i class="fa fa-angle-down"></i></a>';
+																		echo '</li>';
+																}
+																?>
+														</ul>
+												</div>
+
+                        <div id="slicknav-mobile"></div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	<div id="content" class="site-content">
